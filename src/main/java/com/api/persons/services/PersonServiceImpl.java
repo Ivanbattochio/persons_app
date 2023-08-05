a34ca9772fda8b7d4b8fa3b202642c53062f8b8f
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-08-05 19:19:05 -0300

    feat: transactional annotation for PersonService

--- src/main/java/com/api/persons/services/PersonServiceImpl.java
+++ src/main/java/com/api/persons/services/PersonServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
