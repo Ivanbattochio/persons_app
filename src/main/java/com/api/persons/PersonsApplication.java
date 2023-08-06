cb5df092aa60f8e7f6ab411dcef824be80a64cd4
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-08-06 18:45:06 -0300

    fix: renamed crud to person, chore: added spring docs and h2database for testing

--- src/main/java/com/api/persons/PersonsApplication.java
+++ src/main/java/com/api/persons/PersonsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class PersonsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrudApplication.class, args);
+        SpringApplication.run(PersonsApplication.class, args);
     }
 
 }
